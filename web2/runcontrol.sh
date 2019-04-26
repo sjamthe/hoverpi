28d0c0072ca2d616e45699e4cdcff2874712fc34
Shirish Jamthe <sjamthe@yahoo.com>
2019-04-25 22:52:53 -0700

    fixed LD_LIBRARY_PATH

--- web2/runcontrol.sh
+++ web2/runcontrol.sh
@@ -8,7 +8,7 @@ exit_script() {
 trap exit_script SIGINT SIGTERM
 
 APP_HOME='/home/pi/src/hoverpi/web2'
-export LD_LIBRARY_PATH=../mjpg-streamer-experimental
+export LD_LIBRARY_PATH=$APP_HOME/../mjpg-streamer-experimental
 $APP_HOME/../mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w $APP_HOME/../mjpg-streamer-experimental/www" -i "input_raspicam.so -x 400 -y 400 -fps 30 -ex night" &
 env FLASK_APP=$APP_HOME/control.py flask run --host=0.0.0.0
 kill %1
